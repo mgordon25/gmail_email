3de5c42088094cb6549ed5652877de1009e3656a
mgordon25 <mgordon25@gmail.com>
2019-11-16 13:06:04 -0800

    added html into email

--- test_email.py
+++ test_email.py
@@ -1,37 +1,71 @@
 import smtplib
 import getpass
+from email.message import EmailMessage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-gmail_user = 'mgdevtest@gmail.com'
-#gmail_password = ''
+
+
+sender_email = 'mgdevtest@gmail.com'
+receiver_email = 'mgordon25@gmail.com'
+gmail_password = 'fvfu3533'
 
 
 
-try:
-    gmail_password = input("Password: ")
+#try:
+#    gmail_password = input("Password: ")
     
-except:
-    print("error")
+#except:
+#    print("error")
 
 #TODO -- better method of password input and masking
 #TODO -- add html
 
-sent_from = gmail_user
-to = ['mgordon25@gmail.com']
-subject = 'Daily report'
-body = 'Here is the daily report'
+message = MIMEMultipart("alternative")
+message["Subject"] = "multipart test"
+message["From"] = sender_email
+message["To"] = receiver_email
+
+#sent_from = gmail_user
+#to = ['mgordon25@gmail.com']
+#subject = 'Daily report'
+#body = 'Here is the daily report'
 
 email_text = """\
-From: %s
-To: %s
-Subject: %s
+From: 
+To: 
+Subject: 
 
-%s
-""" %(sent_from, ", ".join(to), subject, body)
+
+""" #%(sent_from, ", ".join(to), subject, body)
+
+html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       <a href="http://www.realpython.com">Real Python</a> 
+       has many great tutorials.
+    </p>
+  </body>
+</html>
+"""
+
+# convert to plain/html MIMEText objects
+part1 = MIMEText(email_text, "plain")
+part2 = MIMEText(html, "html")
+
+# Add HTML/plain text to MIMEMultipart message
+# the email client will try to render the last part first
+
+message.attach(part1)
+message.attach(part2)
+
 try:
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.ehlo()
-    server.login(gmail_user, gmail_password)
-    server.sendmail(sent_from, to , email_text)
+    server.login(sender_email, gmail_password)
+    server.sendmail(sender_email, receiver_email , message.as_string())
     server.close()
     
 
